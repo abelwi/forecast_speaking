6e4997031ccc78f7dba54d9d122af749bb357593
Lam <huyenhuong3010@gmail.com>
2024-11-24 04:06:43 +0000

    add tailwind to sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="font-bold">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="font-bold">This is lam</div>  

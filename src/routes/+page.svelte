d2e8eb59d306dcdb073176e23d4de2504b223360
Lam <huyenhuong3010@gmail.com>
2024-11-26 06:31:40 +0000

    style part1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,25 +5,6 @@
   let activeSection = 0;
   let isClicking = false; // To temporarily disable scroll logic
 
-  onMount(() => {
-    const handleScroll = () => {
-      if (isClicking) return; // Ignore scroll events while clicking
-
-      const sections = document.querySelectorAll("[id^='section-']");
-      sections.forEach((section, index) => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top <= 300 && rect.bottom >= 300) {
-          activeSection = index;
-        }
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  });
-
   const handleClick = (index: number) => {
     isClicking = true; // Temporarily disable scroll logic
     activeSection = index;
@@ -34,7 +15,7 @@
     // Re-enable scroll handling after scrolling finishes
     setTimeout(() => {
       isClicking = false;
-    }, 500); // Adjust timeout based on scrolling duration
+    }, 2000); // Adjust timeout based on scrolling duration
   };
 </script>
 
@@ -63,19 +44,19 @@
     {/each}
   </div>
 
-  <div class="px-20 flex justify-center">
+  <div class="px-10 sm:px-20 flex justify-center">
     <div>
       {#each part1Forecast as item, i}
-        <div id="section-{i}" class="h-5"></div>
+        <div id="section-{i}" class="h-16"></div>
         <div 
-          class="text-xl flex font-medium mt-10 mb-5 border border-base-300 rounded-lg p-4 shadow-sm hover:shadow-md 
-          transition-shadow duration-300 bg-primary-content"
+          class="text-xl flex font-medium  mt-5 mb-5 border border-base-300 rounded-lg px-4 py-3 shadow-sm hover:shadow-md 
+          transition-shadow duration-300 bg-primary-content space-x-7"
         >
-          <img src="src/images/title.png" alt="Title" class="w-9 h-9 mr-4">
+          <img src="src/images/title.png" alt="Title" class="w-9 h-9">
           <span class="mt-1">{item.title}</span>
         </div>
 
-        <div class="mb-10 space-y-5 ml-10">
+        <div class="mb-2 space-y-5 ml-5 sm:ml-10">
           {#each item.questions as question}
             <div class="flex space-x-2">
               <img src="src/images/question.png" alt="Question" class="w-7 h-7">

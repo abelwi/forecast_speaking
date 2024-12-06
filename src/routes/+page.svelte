9c8d1c2928c3dfb1f3562dbca598efedd35b4c1a
Lam <huyenhuong3010@gmail.com>
2024-12-06 02:21:12 +0000

    style home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
   
 </script>
  
-<div class="text-center mb-10 mt-16">
+<div class="text-center mt-32 mb-5">
   <h1 class="text-4xl">Bộ đề dự đoán 
-    <strong class="tooltip tooltip-right" 
+    <strong class="" 
     data-tip="Dự đoán trước các chủ đề hoặc câu hỏi có khả năng xuất hiện ở phần thi IELTS Speaking"
     >
       <span class="text-primary">IELTS SPEAKING FORECAST</span> 
@@ -12,110 +12,123 @@
     theo tháng! <em>(Tháng 12)</em>
   </h1>
   <p class="text-gray-500 font-medium mt-4">Tổng hợp Forecast theo từng tháng để các bạn có thể dễ dàng theo dõi.</p>
-</div>
 
-<div class="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
-  <h2 class="text-2xl text-neutral font-semibold text-center mb-6">Vai trò của speaking forecast trong IELTS</h2>
-  
-  <!-- Roles Grid -->
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-      <!-- Role 1: Dự đoán các chủ đề thường xuất hiện -->
-      <div class="border-2 border-primary-content p-4 rounded-lg shadow-md text-center">
-          <div class="flex justify-center mb-4">
-            <img src="src/images/bookmark.png" alt="circle_tag" class="w-3 h-3">
-          </div>
-          <h3 class="text-xl text-primary font-semibold mb-2">Dự đoán các chủ đề thường xuất hiện</h3>
-          <p class="text-sm text-gray-600">Dự đoán các chủ đề có thể xuất hiện trong bài thi Speaking để chuẩn bị tốt hơn.</p>
-      </div>
-
-      <!-- Role 2: Chuẩn bị từ vựng và cấu trúc ngữ pháp -->
-      <div class="border-2 border-primary-content p-4 rounded-lg shadow-md text-center">
-          <div class="flex justify-center mb-4">
-            <img src="src/images/bookmark.png" alt="circle_tag" class="w-3 h-3">
-          </div>
-          <h3 class="text-xl text-primary font-semibold mb-2">Chuẩn bị từ vựng và cấu trúc ngữ pháp</h3>
-          <p class="text-sm text-gray-600">Giúp bạn học từ vựng và cấu trúc ngữ pháp phù hợp với các chủ đề thường gặp.</p>
-      </div>
-
-      <!-- Role 3: Xây dựng ý tưởng và câu trả lời mẫu -->
-      <div class="border-2 border-primary-content p-4 rounded-lg shadow-md text-center">
-          <div class="flex justify-center mb-4">
-              <img src="src/images/bookmark.png" alt="circle_tag" class="w-3 h-3">
-          </div>
-          <h3 class="text-xl text-primary font-semibold mb-2">Xây dựng ý tưởng và câu trả lời mẫu</h3>
-          <p class="text-sm text-gray-600">Cung cấp cách xây dựng ý tưởng và trả lời mẫu để dễ dàng tham gia vào các phần của bài thi.</p>
-      </div>
-
-      <!-- Role 4: Tăng cường sự tự tin -->
-      <div class="border-2 border-primary-content p-4 rounded-lg shadow-md text-center">
-          <div class="flex justify-center mb-4">
-            <img src="src/images/bookmark.png" alt="circle_tag" class="w-3 h-3">
-          </div>
-          <h3 class="text-xl text-primary font-semibold mb-2">Tăng cường sự tự tin</h3>
-          <p class="text-sm text-gray-600">Giúp bạn tự tin hơn khi tham gia phần Speaking của kỳ thi IELTS.</p>
-      </div>
+  <div class="flex justify-center mt-16 space-x-5">
+    <a href="/part1" class="btn btn-primary btn-outline btn-lg btn-wide">Part1</a>
+    <a href="/part2-3" class="btn btn-outline btn-primary btn-lg btn-wide">Part 2&3</a>
   </div>
 </div>
 
-<div class="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-20 mb-10">
-  <h2 class="text-2xl text-neutral font-semibold text-center mb-6">Cấu trúc bài thi IELTS Speaking</h2>
-  
-  <!-- Step Diagram -->
-  <div class="space-y-8">
-      <!-- Part 1 -->
-      <div class="flex items-center space-x-4">
-          <div class="w-16 h-16 bg-primary text-white flex items-center justify-center rounded-full">
-              <span class="text-lg font-semibold">1</span>
-          </div>
-          <div>
-              <h3 class="text-xl font-semibold">Part 1: Introduction & Interview</h3>
-              <p class="text-sm text-gray-600">Thời gian: 4-5 phút. Câu hỏi về các chủ đề quen thuộc (ví dụ: sở thích, quê hương).</p>
-          </div>
-      </div>
-      
-      <!-- Arrow -->
-      <div class="flex justify-center">
-          <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-          </svg>
-      </div>
 
-      <!-- Part 2 -->
-      <div class="flex items-center space-x-4">
-          <div class="w-16 h-16 bg-secondary text-white flex items-center justify-center rounded-full">
-              <span class="text-lg font-semibold">2</span>
-          </div>
-          <div>
-              <h3 class="text-xl font-semibold">Part 2: Individual long Turn</h3>
-              <p class="text-sm text-gray-600">Thời gian: 3-4 phút (bao gồm 1 phút chuẩn bị). 
-                Nói về một chủ đề nhất định.
-              </p>
-          </div>
-      </div>
+<div class="collapse">
+  <input type="checkbox" />
+  <div class="collapse-title">
+    <div class="flex justify-center space-x-3">
+      <p class="text-xl font-medium btn btn-primary">
+        Tìm hiểu thêm
+      </p>
+      <img src="src/images/icons8-double-down.gif" alt="double_down">
+    </div>
+  </div>
+  <div class="collapse-content mt-5">
+    <div class="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+      <h2 class="text-2xl font-semibold text-center mb-6">Vai trò của speaking forecast trong IELTS</h2>
       
-      <!-- Arrow -->
-      <div class="flex justify-center">
-          <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-          </svg>
-      </div>
-
-      <!-- Part 3 -->
-      <div class="flex items-center space-x-4">
-          <div class="w-16 h-16 bg-accent text-white flex items-center justify-center rounded-full">
-              <span class="text-lg font-semibold">3</span>
+      <!-- Roles Grid -->
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <!-- Role 1: Dự đoán các chủ đề thường xuất hiện -->
+          <div class="border-2 border-secondary-content p-4 rounded-lg shadow-md text-center">
+              <div class="flex justify-center mb-4">
+                <img src="src/images/bookmark.png" alt="circle_tag" class="w-3 h-3">
+              </div>
+              <h3 class="text-xl text-neutral font-semibold mb-2">Dự đoán các chủ đề thường xuất hiện</h3>
+              <p class="text-sm text-gray-600">Dự đoán các chủ đề có thể xuất hiện trong bài thi Speaking để chuẩn bị tốt hơn.</p>
           </div>
-          <div>
-              <h3 class="text-xl font-semibold">Part 3: Discussion</h3>
-              <p class="text-sm text-gray-600">Thời gian: 4-5 phút. 
-                Giám khảo sẽ hỏi thêm những câu hỏi phức tạp hơn liên quan đến chủ đề ở Part 2.
-              </p>
+    
+          <!-- Role 2: Chuẩn bị từ vựng và cấu trúc ngữ pháp -->
+          <div class="border-2 border-secondary-content p-4 rounded-lg shadow-md text-center">
+              <div class="flex justify-center mb-4">
+                <img src="src/images/bookmark.png" alt="circle_tag" class="w-3 h-3">
+              </div>
+              <h3 class="text-xl text-neutral font-semibold mb-2">Chuẩn bị từ vựng và cấu trúc ngữ pháp</h3>
+              <p class="text-sm text-gray-600">Giúp bạn học từ vựng và cấu trúc ngữ pháp phù hợp với các chủ đề thường gặp.</p>
+          </div>
+    
+          <!-- Role 3: Xây dựng ý tưởng và câu trả lời mẫu -->
+          <div class="border-2 border-secondary-content p-4 rounded-lg shadow-md text-center">
+              <div class="flex justify-center mb-4">
+                  <img src="src/images/bookmark.png" alt="circle_tag" class="w-3 h-3">
+              </div>
+              <h3 class="text-xl text-neutral font-semibold mb-2">Xây dựng ý tưởng và câu trả lời mẫu</h3>
+              <p class="text-sm text-gray-600">Cung cấp cách xây dựng ý tưởng và trả lời mẫu để dễ dàng tham gia vào các phần của bài thi.</p>
+          </div>
+    
+          <!-- Role 4: Tăng cường sự tự tin -->
+          <div class="border-2 border-secondary-content p-4 rounded-lg shadow-md text-center">
+              <div class="flex justify-center mb-4">
+                <img src="src/images/bookmark.png" alt="circle_tag" class="w-3 h-3">
+              </div>
+              <h3 class="text-xl text-neutral font-semibold mb-2">Tăng cường sự tự tin</h3>
+              <p class="text-sm text-gray-600">Giúp bạn tự tin hơn khi tham gia phần Speaking của kỳ thi IELTS.</p>
           </div>
       </div>
+    </div>
+    
+    <div class="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-20 mb-10">
+      <h2 class="text-2xl text-neutral font-semibold text-center mb-6">Cấu trúc bài thi IELTS Speaking</h2>
+      
+      <!-- Step Diagram -->
+      <div class="space-y-8">
+          <!-- Part 1 -->
+          <div class="flex items-center space-x-4">
+              <div class="w-16 h-16 bg-primary text-white flex items-center justify-center rounded-full">
+                  <span class="text-lg font-semibold">1</span>
+              </div>
+              <div>
+                  <h3 class="text-xl font-semibold">Part 1: Introduction & Interview</h3>
+                  <p class="text-sm text-gray-600">Thời gian: 4-5 phút. Câu hỏi về các chủ đề quen thuộc (ví dụ: sở thích, quê hương).</p>
+              </div>
+          </div>
+          
+          <!-- Arrow -->
+          <div class="flex justify-center">
+              <img src="src/images/down-arrow.png" alt="Down arrow" class="w-5 h-5">
+          </div>
+    
+          <!-- Part 2 -->
+          <div class="flex items-center space-x-4">
+              <div class="w-16 h-16 bg-secondary text-white flex items-center justify-center rounded-full">
+                  <span class="text-lg font-semibold">2</span>
+              </div>
+              <div>
+                  <h3 class="text-xl font-semibold">Part 2: Individual long Turn</h3>
+                  <p class="text-sm text-gray-600">Thời gian: 3-4 phút (bao gồm 1 phút chuẩn bị). 
+                    Nói về một chủ đề nhất định.
+                  </p>
+              </div>
+          </div>
+          
+          <!-- Arrow -->
+          <div class="flex justify-center">
+            <img src="src/images/down-arrow.png" alt="Down arrow" class="w-5 h-5">
+          </div>
+    
+          <!-- Part 3 -->
+          <div class="flex items-center space-x-4">
+              <div class="w-16 h-16 bg-accent text-white flex items-center justify-center rounded-full">
+                  <span class="text-lg font-semibold">3</span>
+              </div>
+              <div>
+                  <h3 class="text-xl font-semibold">Part 3: Discussion</h3>
+                  <p class="text-sm text-gray-600">Thời gian: 4-5 phút. 
+                    Giám khảo sẽ hỏi thêm những câu hỏi phức tạp hơn liên quan đến chủ đề ở Part 2.
+                  </p>
+              </div>
+          </div>
+      </div>
+    </div>
   </div>
 </div>
 
-<div class="flex justify-center mb-20 pt-3 space-x-5">
-  <a href="/part1" class="btn btn-primary btn-outline btn-lg btn-wide">Part1</a>
-  <a href="/part2-3" class="btn btn-outline btn-primary btn-lg btn-wide">Part 2&3</a>
-</div>
+
+
